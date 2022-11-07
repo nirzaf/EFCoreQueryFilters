d4b547e0c86745571846c9d273b6cd54d0dac310
Mohamed Fazrin <mfmfazrin1986@gmail.com>
2022-11-07 14:32:23 +0300

    Commit changes

--- Models/EventDbContext.cs
+++ Models/EventDbContext.cs
@@ -21,15 +21,15 @@ public class EventDbContext : DbContext
             .RuleFor(m=>m.UpdatedAt , f=> DateTime.UtcNow)
             .RuleFor(m=>m.CreatedBy , f=> f.Person.UserName)
             .RuleFor(m=>m.UpdatedBy , f=> f.Person.UserName)
-            .RuleFor(m=>m.eventCode, f=>f.Random.AlphaNumeric(15))
+            .RuleFor(m=>m.eventCode, f=>f.Random.AlphaNumeric(15).ToUpper())
             .RuleFor(m=>m.eventName, f=>f.Name.FirstName())
             .RuleFor(m=>m.eventDescription, f=>f.Lorem.Sentence())
             .RuleFor(m=>m.eventLocation, f=>f.Address.City())
             .RuleFor(m=>m.eventStartDate, f=>f.Date.Past())
-            .RuleFor(m=>m.eventEndDate, f=>f.Date.Past())
+            .RuleFor(m=>m.eventEndDate, f=>f.Date.Soon())
             .RuleFor(m=>m.eventStatus, f=> f.PickRandom("Active", "Inactive", "Cancelled", "Completed"));
         
-        modelBuilder.Entity<Event>().HasData(eventNew.GenerateBetween(1000, 2000));
+        modelBuilder.Entity<Event>().HasData(eventNew.GenerateBetween(500, 1000));
         
         modelBuilder.Entity<Event>()
             .HasQueryFilter(e => e.IsDeleted == false);
